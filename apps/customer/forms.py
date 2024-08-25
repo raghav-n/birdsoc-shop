0bab959836d30935b6855e83890c85b990ab73f3
raghav-n <raghav_n@outlook.com>
2024-08-25 22:22:30 +0800

    Add PDPA agreement to registration form

--- apps/customer/forms.py
+++ apps/customer/forms.py
@@ -1,4 +1,7 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit
 from django import forms
+from django.utils.safestring import mark_safe
 from oscar.apps.customer.forms import EmailUserCreationForm as CoreEmailUserCreationForm
 from oscar.apps.customer.forms import ProfileForm as CoreProfileForm
 from oscar.core.compat import get_user_model
@@ -9,13 +12,27 @@ User = get_user_model()
 class EmailUserCreationForm(CoreEmailUserCreationForm):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
+    pdpa_agreement = forms.BooleanField(
+        label=mark_safe(
+            "I have read and agree to the <a href='https://birdsociety.sg/data-protection-notice/'>data protection notice</a>."
+        )
+    )
 
     class Meta:
         model = User
-        fields = (
-            "email",
-            "first_name",
-            "last_name",
+        fields = ("email", "first_name", "last_name", "pdpa_agreement")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Field("first_name"),
+            Field("last_name"),
+            Field("email"),
+            Field("password1"),
+            Field("password2"),
+            Field("pdpa_agreement"),
+            Submit("registration_submit", "Register"),
         )
 
     def save(self, commit=True):
