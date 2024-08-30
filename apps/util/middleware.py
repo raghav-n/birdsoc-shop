a11890866de484d5446d8b9163ca9ba54363b45d
raghav-n <raghav_n@outlook.com>
2024-08-30 19:09:06 +0800

    Fix LoginRequiredMiddleware to allow forgot password view

--- apps/util/middleware.py
+++ apps/util/middleware.py
@@ -7,7 +7,13 @@ class LoginRequiredMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.login_url = settings.LOGIN_ONLY_URL
-        self.open_urls = [self.login_url, reverse("password-reset")] + getattr(settings, "OPEN_URLS", [])
+        self.open_urls = ([
+            self.login_url,
+            reverse("password-reset"),
+            reverse("password-reset-done"),
+            "password-reset/confirm/",
+            reverse("password-reset-complete")
+        ] + getattr(settings, "OPEN_URLS", []))
 
     def __call__(self, request):
         if (
