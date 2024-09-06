309ffbd669eac78b16e53cd0f40209cddad366e0
raghav-n <raghav_n@outlook.com>
2024-09-06 11:33:20 +0800

    Add donation info for checkout page

--- apps/checkout/extra_forms.py
+++ apps/checkout/extra_forms.py
@@ -11,12 +11,19 @@ Source = get_model("payment", "Source")
 
 class PayNowDetailsForm(forms.ModelForm):
     donation = forms.IntegerField(
-        required=False, label=mark_safe("<strong>Add a donation (optional)</strong>")
+        required=False, label=mark_safe("<strong><u><a href='https://birdsociety.sg/support-us/' target='_blank'>Add a donation</a></u> (optional)</strong>"),
+        help_text="<span class='text-warning font-weight-bold'>"
+                      "Learn more about donating to the Bird Society of Singapore "
+                      "<u><a href='https://birdsociety.sg/support-us/' target='_blank'>here</a></u>."
+                  "</span>"
     )
     reference = forms.CharField(
         required=False,
         label=mark_safe("<strong>Payment reference</strong>"),
-        help_text="<strong class='text-warning'>Please enter this reference number when making your payment, so that your payment can be easily traced.</strong>",
+        help_text="<span class='text-info font-weight-bold'>"
+                      "Please enter this reference number when making your payment, "
+                      "so that your payment can be easily traced."
+                  "</span>",
     )
     payment_proof = forms.ImageField(
         required=True,
