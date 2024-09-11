eda915b1ad7eba99a777e0ad4476d2704966e491
raghav-n <raghav_n@outlook.com>
2024-09-11 13:50:15 +0800

    Add 10 Nov date to website

--- apps/shipping/methods.py
+++ apps/shipping/methods.py
@@ -9,11 +9,11 @@ class SelfCollectHW2024(methods.NoShippingRequired):
     code = "SELFCOLLECTHW2024"
     name = "Self-collect at Henderson Waves Raptor Watch: 2024"  # must be unique
     description = (
-        "<p>Please collect your merchandise from our booth at Henderson Waves on 20 October, 27 October, or 3 November.</p>"
-        "<p><strong>Unfortunately, we are unable to offer refunds for items left unclaimed after 3 November.</strong></p>"
+        "<p>Please collect your merchandise from our booth at Henderson Waves on 20 October, 27 October, 3 November, or 10 November.</p>"
+        "<p><strong>Unfortunately, we are unable to offer refunds for items left unclaimed after 10 November.</strong></p>"
     )
     email_description = (
-        "Your items can be collected from our Raptor Watch booth at Henderson Waves on 20 October, 27 October, and 3 November. "
+        "Your items can be collected from our Raptor Watch booth at Henderson Waves on 20 October, 27 October, 3 November, or 10 November. "
         "Please come to our booth to collect items on any of these three dates; we will be in touch to share collection details in October. "
         "Unfortunately, we are unable to offer refunds for items left unclaimed after the three Raptor Watch dates at Henderson Waves."
     )
@@ -22,9 +22,10 @@ class SelfCollectHW2024(methods.NoShippingRequired):
         f"<strong>For this round of merchandise sales, we're accepting orders from now until {end_date.strftime('%d %b %Y')}</strong>. Collection will be at Henderson Waves, "
         "where we'll be hosting the third edition of our Raptor Watch booth on these dates:"
         "<ul class='mt-2 pl-4 mb-2'>"
-        "<li>20 October</li>"
-        "<li>27 October</li>"
-        "<li>3 November</li>"
+            "<li>20 October</li>"
+            "<li>27 October</li>"
+            "<li>3 November</li>"
+            "<li>10 November</li>"
         "</ul>"
         "This event is <a class='font-weight-bold' href='https://birdsociety.sg/2023/11/09/henderson-waves-raptor-watch-2023/' target='_blank'>always really popular</a>, "
         "and you can join us for a session of raptor watching and collect your merchandise at the same time! BirdSoc SG is a fully volunteer-run "
