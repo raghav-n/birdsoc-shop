f77e7246aa2d59464b414094e10a6b4d8d096d2f
raghav-n <raghav_n@outlook.com>
2024-09-11 19:57:27 +0800

    Add event time details to FAQ and other places

--- apps/shipping/methods.py
+++ apps/shipping/methods.py
@@ -9,18 +9,18 @@ class SelfCollectHW2024(methods.NoShippingRequired):
     code = "SELFCOLLECTHW2024"
     name = "Self-collect at Henderson Waves Raptor Watch: 2024"  # must be unique
     description = (
-        "<p>Please collect your merchandise from our booth at Henderson Waves on 20 October, 27 October, 3 November, or 10 November.</p>"
+        "<p>Please collect your merchandise from our booth at Henderson Waves between 9:00am and noon on 20 October, 27 October, 3 November, or 10 November.</p>"
         "<p><strong>Unfortunately, we are unable to offer refunds for items left unclaimed after 10 November.</strong></p>"
     )
     email_description = (
-        "Your items can be collected from our Raptor Watch booth at Henderson Waves on 20 October, 27 October, 3 November, or 10 November. "
-        "Please come to our booth to collect items on any of these three dates; we will be in touch to share collection details in October. "
-        "Unfortunately, we are unable to offer refunds for items left unclaimed after the three Raptor Watch dates at Henderson Waves."
+        "Your items can be collected from our Raptor Watch booth at Henderson Waves between 9:00am and noon on 20 October, 27 October, 3 November, or 10 November. "
+        "Please come to our booth to collect items on any of these four dates; we will be in touch to share collection details in October. "
+        "Unfortunately, we are unable to offer refunds for items left unclaimed after the four Raptor Watch dates at Henderson Waves."
     )
     website_home_description = (
         "<div class='alert alert-info mt-0'>"
         f"<strong>For this round of merchandise sales, we're accepting orders from now until {end_date.strftime('%d %b %Y')}</strong>. Collection will be at Henderson Waves, "
-        "where we'll be hosting the third edition of our Raptor Watch booth on these dates:"
+        "where we'll be hosting the third edition of our Raptor Watch booth on these dates, from <strong>9:00am to noon:</strong>"
         "<ul class='mt-2 pl-4 mb-2'>"
             "<li>20 October</li>"
             "<li>27 October</li>"
