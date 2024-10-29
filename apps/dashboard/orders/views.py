3e781c2567b6b71f793cc8925f20a3e2e89d9bc4
raghav-n <raghav_n@outlook.com>
2024-10-29 09:24:20 +0800

    Add OrderExportView

--- apps/dashboard/orders/views.py
+++ apps/dashboard/orders/views.py
@@ -1,12 +1,20 @@
 import json
+from io import BytesIO
 
 from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth import login
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
 from oscar.core.loading import get_model
+
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import user_passes_test
+
+import pandas as pd
+from django.http import HttpResponse
 
 
 @method_decorator(staff_member_required, name="dispatch")
@@ -58,3 +66,58 @@ class OrderCollectionView(View):
             )
 
         return JsonResponse({"success": True})
+
+
+@method_decorator(user_passes_test(lambda u: u.is_superuser), name="dispatch")
+class OrderExportView(View):
+    def get(self, request, *args, **kwargs):
+        Line = get_model("order", "Line")
+        df = pd.DataFrame(
+            Line._default_manager.select_related("order").values(
+                "order__number",
+                "order__user__first_name",
+                "order__user__last_name",
+                "order__status",
+                "title",
+                "quantity",
+            )
+        )
+        df["quantity x item"] = (
+            df["quantity"].astype(str)
+            + "x "
+            + df["title"].str.replace("\[[a-zA-Z ]+\]", "", regex=True)
+        ).str.strip()
+
+        grouped = df.groupby("order__number").agg(
+            {
+                "order__user__first_name": "first",
+                "order__user__last_name": "first",
+                "order__status": "first",
+                "quantity x item": "\n".join,
+            }
+        )
+
+        grouped["order__user__name"] = (
+            grouped["order__user__first_name"] + " " + grouped["order__user__last_name"]
+        )
+        grouped = grouped.drop(
+            columns=["order__user__first_name", "order__user__last_name"]
+        )
+        grouped.index = grouped.index.astype(int)
+
+        output = BytesIO()
+        # Save the DataFrame to the output stream as an Excel file
+        grouped[["order__user__name", "order__status", "quantity x item"]].to_excel(
+            output, index=True, engine="openpyxl"
+        )
+
+        # Set the stream position to the beginning
+        output.seek(0)
+
+        # Create the HTTP response with the Excel file as an attachment
+        response = HttpResponse(
+            output,
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        )
+        response["Content-Disposition"] = 'attachment; filename="exported_data.xlsx"'
+        return response
